66902092ccc84b4caa13133c278635e837996d62
Aytenkcbyk <104267260+Aytenkcbyk@users.noreply.github.com>
2024-01-22 18:50:45 +0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+
+  modules:[
+    '@pinia/nuxt',
+    '@kevinmarrec/nuxt-pwa',
+    'nuxt-vitest'
+  ],
+  pwa: {
+    meta: {
+      title: "Ebay",
+      author: "Me"
+    },
+    manifest: {
+      name: "ebay",
+      short_name: "ebay",
+      lang: "en",
+      theme_color: "#ffffff"
+    },
+    workbox: {
+      enabled: true
+    }  
+  }
+})
